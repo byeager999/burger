83d2d637828677db2e63cc5d01fe57d7b717eaf4
Brian Yeager <rbrianyeager@gmail.com>
2019-10-28 21:10:27 -0400

    update the port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var PORT = process.env.PORT || 3306;
-
+// here's some express stuff
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
